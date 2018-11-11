ba934b1120c7155a90c212a14e88a621049f39b1
rjewsbury <ryleyj@shaw.ca>
2018-11-11 15:38:41 -0800

    changed initial sampling to random

--- colorclusters/k_means.py
+++ colorclusters/k_means.py
@@ -1,38 +1,43 @@
 from .distance import euclidean
 from math import inf
+from random import randint
 import colorclusters.image_utils as utils
+
 
 def _choose_initial_centroids(k_value, datapoints):
     if not datapoints:
         return []
-    centroids = [datapoints[0]]*k_value
+    centroids = [datapoints[randint(0, len(datapoints))] for i in range(k_value)]
     return centroids
 
+
 class KMeans():
-    def __init__(self, k_value, datapoints, distance = euclidean):
+    def __init__(self, k_value, datapoints, distance=euclidean):
         # to prevent things breaking on empty data, adds one point
         if len(datapoints) == 0:
-            datapoints = [(0,0,0)]
+            datapoints = [(0, 0, 0)]
 
         self.k_value = k_value
         self.dimensions = len(datapoints[0])
-        self.shift_distance = [inf]*k_value
+        self.shift_distance = [inf] * k_value
         self.data = datapoints
         self.dist = distance
         self.clustering = None
         self.error = None
         self.centroids = _choose_initial_centroids(k_value, datapoints)
 
-    def compute_until_predicate(self, predicate):
+    def compute_until_predicate(self, predicate, debug=False):
         while not predicate(self):
             self.shift_centroids()
+            if debug:
+                print(self.shift_distance)
 
-    def compute_until_max_distance(self, max_distance):
-        self.compute_until_predicate(lambda self: max(self.shift_distance) <= max_distance)
+    def compute_until_max_distance(self, max_distance, debug=False):
+        self.compute_until_predicate(lambda self: max(self.shift_distance) <= max_distance, debug)
 
     def shift_centroids(self):
-        centroids = [[0]*self.dimensions for i in range(self.k_value)]
-        count = [0]*self.k_value
+        centroids = [[0] * self.dimensions for i in range(self.k_value)]
+        count = [0] * self.k_value
 
         for i, pixel in zip(self.get_clustering(), self.data):
             count[i] += 1
@@ -49,7 +54,7 @@ class KMeans():
 
             for d in range(self.dimensions):
                 centroids[i][d] //= count[i]
-            self.shift_distance[i] = self.dist(self.centroids[i],centroids[i])
+            self.shift_distance[i] = self.dist(self.centroids[i], centroids[i])
 
         # update the centroids to the new averages
         self.centroids = centroids
--- tests/test_kmeans.py
+++ tests/test_kmeans.py
@@ -1,25 +1,19 @@
 from PIL import Image
-import math
 from colorclusters import distance
 from colorclusters.image_utils import map_index_to_paletted_image
 from colorclusters.k_means import KMeans
+from timeit import default_timer
 
-# creates a gradient image
-img = Image.new('RGB', (255, 255))
-img.putdata([
-    (int(255 * math.sin((x - y)/256))
-     ,int(255 * math.cos((x + y)/256))
-     ,int(255 * math.sin((x + y)/256))
-     #,(abs((y - 127)) + abs(x - 127))//4+170
-    )
-    for x in range(255) for y in range(255)])
-img.save("./images/pre_kmean_test.png")
+img = Image.open("./images/flower.png")
 
-algorithm = KMeans(4,list(img.getdata()),distance.euclidean)
+algorithm = KMeans(16,list(img.getdata()),distance.euclidean)
 
-algorithm.compute_until_max_distance(5)
+start = default_timer()
+algorithm.compute_until_max_distance(5, True)
+end = default_timer()
+print((end-start)//60,"minutes",round((end-start)%60,3),"seconds")
 
 img = map_index_to_paletted_image(img, algorithm.get_clustering(), algorithm.get_centroids())
 
 # save the indexed result
-img.save("./images/post_kmean_test.png")
+img.save("./images/post_flower_test.png")
